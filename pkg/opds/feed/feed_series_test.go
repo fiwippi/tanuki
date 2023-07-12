f690a89ac606844647580f7604535e5bcf2f884f
nadav rahimi <nadavrahimi@gmail.com>
2023-07-12 21:26:58 +0100

    fix test files

--- pkg/opds/feed/feed_series_test.go
+++ pkg/opds/feed/feed_series_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/fiwippi/tanuki/internal/platform/archive"
+	"github.com/fiwippi/tanuki/internal/archive"
 	"github.com/fiwippi/tanuki/pkg/manga"
 )
 
